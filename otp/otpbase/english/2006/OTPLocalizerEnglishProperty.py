2d19df2d2eef5f1fffa9fc55a0e61a7137da82ca
Vitor25laser <97393601+Vitor23laser@users.noreply.github.com>
2024-06-10 08:32:16 -0300

    Create OTPLocalizerEnglishProperty.py

---
+++ otp/otpbase/english/2006/OTPLocalizerEnglishProperty.py
@@ -0,0 +1,15 @@
+ADPtrackLabel = 0.066
+ADPcancelButton = 0.05
+APfriendButton = 0.06
+APwhisperButton = 0.06
+APsecretsButton = 0.06
+APgoToButton = 0.06
+APignoreButton = 0.06
+FSenterSecretTextPos = (0, 0, -0.27)
+FSgotSecretPos = (0, 0, 0.37)
+FSgetSecretButton = 0.06
+FSnextText = 1.0
+LTPDdirectButtonYesText = 0.05
+LTPDdirectButtonNoText = 0.05
+LTPDdirectFrameText = 0.06
+SCOsubmenuOverlap = 2.0 / 3
