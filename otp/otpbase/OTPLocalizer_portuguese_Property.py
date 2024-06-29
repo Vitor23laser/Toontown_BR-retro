91eae615b370fa7ff3f7aa8d061ef9e79ece74db
Vitor25laser <97393601+Vitor23laser@users.noreply.github.com>
2024-06-29 14:12:26 -0300

    Update OTPLocalizer_portuguese_Property.py

--- otp/otpbase/OTPLocalizer_portuguese_Property.py
+++ otp/otpbase/OTPLocalizer_portuguese_Property.py
@@ -1,4 +1,3 @@
-#friends/FriendSecret.py
 FSenterSecretTextPos = (0, 0, -0.22)
 FSgotSecretPos = (0, 0, 0.42)
 FSgetSecretButton = 0.045
@@ -7,11 +6,7 @@ FSgetSecret = (1,1,1)
 FSok1 = (1,1,1)
 FSok2 = (0.6,1,1)
 FScancel = (0.6,1,1)
-
-#login/LeaveToPayDialog.py
 LTPDdirectButtonYesText = 0.04
 LTPDdirectButtonNoText = 0.05
 LTPDdirectFrameText = 0.06
-
-#speedchat/SCSettings.py
 SCOsubmenuOverlap = 2.5/3
